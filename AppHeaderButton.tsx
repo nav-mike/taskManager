1a74e52a57dd05033e0f4dbdd9e06c9758a0861d
Mikhail Navrotskii <m.navrotskiy@gmail.com>
2022-03-15 13:07:10 +0200

    add header button

---
+++ AppHeaderButton.tsx
@@ -0,0 +1,18 @@
+import React, { FC } from "react";
+import {
+  HeaderButton,
+  HeaderButtonProps,
+} from "react-navigation-header-buttons";
+import { Ionicons } from "@expo/vector-icons";
+import Colors from "./constants/colors";
+
+const AppHeaderButton: FC<HeaderButtonProps> = (props) => {
+  return (
+    <HeaderButton
+      {...props}
+      IconComponent={Ionicons}
+      iconSize={23}
+      color={Colors.secondary}
+    />
+  );
+};
